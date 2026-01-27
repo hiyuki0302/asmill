9b199f0cbfa44e70a6ce36a4214cc56316be31bc
hiyuki0302 <crypto.streeeet@gmail.com>
2026-01-27 16:24:18 +0900

    2

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import { useState } from "react";
 function App() {
   const item = [{id: 0, name:'Apple'}, {id: 1, name:'Banana'}, {id: 2, name:'Cherry'}, {id: 3, name:'Date'}, {id: 4, name:'Elderberry'}];
  
-  const SelectItems = item.map(a => 
+  const SelectItemes = item.map(a => 
         <option key={a.id}>{a.name}</option>
     );
 
   return(
     <div>
       <select>
-        {SelectItems}
+        {SelectItemes}
       </select>
     </div>
   )
