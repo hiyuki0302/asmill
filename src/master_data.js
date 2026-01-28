301cdaa866c874b69328fa3629c1f9e946632918
hiyuki0302 <crypto.streeeet@gmail.com>
2026-01-28 15:36:14 +0900

    add role

--- src/master_data.js
+++ src/master_data.js
@@ -3,10 +3,10 @@ export const master = [
         id: '01',
         type: 'Ex01', 
         roles: [
-            {code: '01', job: '医師' },
-            {code: '02', job: '看護師' },
-            {code: '03', job: '薬剤師' },
-            {code: '04', job: '事務' },
+            {code: '01', job: 'd' },
+            {code: '02', job: 'n' },
+            {code: '03', job: 'p' },
+            {code: '04', job: 'o' },
         ]
     }
 ]
