9d06163af9510e28c449b56dcb88ef82ce1b2aab
hiyuki0302 <crypto.streeeet@gmail.com>
2026-02-12 16:22:29 +0900

    2-12

---
+++ src/Importer.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import Papa from 'papaparse';
+
+function CsvImporter() {
+    const [data, setData] = useState([]);
+
+    const handleFileUpload = (event) => {
+        const file = event.target.files[0];
+
+        Papa.parse(file, {
+            header: true,  // 最初の行をヘッダーとして扱う
+            complete: (results) => {
+                console.log(results.data);
+                setData(results.data);
+            },
+            error: (error) => {
+                console.error('Error:', error);
+            }
+        });
+    };
+
+  return (
+    <div>
+      <input type="file" accept=".csv" onChange={handleFileUpload} />
+      {data.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              {Object.keys(data[0]).map((key) => (
+                <th key={key}>{key}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, index) => (
+              <tr key={index}>
+                {Object.values(row).map((value, i) => (
+                  <td key={i}>{value}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+}
+
+export default CsvImporter;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Side_bar from "./App";
+import CsvImporter from "./Importer";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <CsvImporter />
   </React.StrictMode>,
 );
